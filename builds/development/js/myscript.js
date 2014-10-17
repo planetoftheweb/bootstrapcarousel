e6a755b4a9fa9eb9a3141d5341250f07511dc20f
Ray Villalobos <ray@planetoftheweb.com>
2014-10-17 00:50:34 -0400

    working fullscreen carousel

---
+++ builds/development/js/myscript.js
@@ -0,0 +1,38 @@
+$(function() {
+  var topoffset = 0; //variable for scrolling effects
+
+	//fullheight
+  var wheight = $( window ).height(); //get the height of the window
+  $('.fullheight').css('height', wheight); //set to window tallness 
+
+  console.log(wheight);
+
+  //adjust height of .fullheight elements on window resize
+  $( window ).resize(function() {
+    var wheight = $( window ).height();
+    $('.fullheight').css('height', wheight); //set to window tallness 
+  });
+
+	$('#features .item img').each(function() {
+		var imgSrc = $(this).attr('src');
+		$(this).parent().css({'background-image': 'url('+imgSrc+')'});
+		$(this).remove();
+	});
+
+	$('body').scrollspy({ target: 'header .navbar-default' })
+
+  //smooth scrolling from css tricks
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top-topoffset+2
+        }, 500);
+        return false;
+      } //target.length
+    } //click function
+  }); //smooth scrolling
+
+}); //function
