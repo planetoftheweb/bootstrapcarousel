af660e5f2ee80c3c8491f4f0b32e9da4e684e485
Ray Villalobos <ray@planetoftheweb.com>
2014-10-26 22:19:18 -0300

    random slides

--- builds/development/js/myscript.js
+++ builds/development/js/myscript.js
@@ -1,40 +1,48 @@
 $(function() {
-  var topoffset = 50; //variable for scrolling effects
+  var topoffset = 50; //variable for scrolling height
 
-  $('.carousel').carousel();
+  // Start Carousel JavaScript
+
+  var slideqty = $("#featured .item").length;
+  var randSlide = Math.floor(Math.random()*slideqty);
 
 	//Make a special class go full screen
   var wheight = $( window ).height(); //get the height of the window
   $('.fullheight').css('height', wheight); //set to window tallness 
 
   //adjust height of .fullheight elements on window resize
-  $( window ).resize(function() {
-    var wheight = $( window ).height();
+  $(window).resize(function() {
+    wheight = $( window ).height();
     $('.fullheight').css('height', wheight); //set to window tallness 
   });
 
   //replace each IMG inside the carousel with a background image
-
 	$('#featured .item img').each(function() {
 		var imgSrc = $(this).attr('src');
 		$(this).parent().css({'background-image': 'url('+imgSrc+')'});
 		$(this).remove();
 	});
 
-//  <!-- Indicators -->
-//  <ol class="carousel-indicators">
-//    <li data-target="#featured" data-slide-to="0" class="active"></li>
-//    <li data-target="#featured" data-slide-to="1"></li>
-//    <li data-target="#featured" data-slide-to="2"></li>
- // </ol>
+  //create the indicators automatically
+  for (var i = 0; i < slideqty; i++) {
+    var insertText = '<li data-target="#featured" data-slide-to="'+ i +'"';
+    if (i == randSlide) {
+      insertText += 'class="active"';
+    }
+    insertText += '></li>';
+    $("#featured ol").append(insertText);
+  };
 
-	//Activate Scrollspy
+  $('#featured .item').eq(randSlide).addClass('active');
+
+  //Carousel JavaScript ends
+
+  //Activate Scrollspy
 	$('body').scrollspy({
     target: 'header .navbar',
     offset: topoffset
   })
-
-
+  
    var hash = $(this).find("li.active a").attr("href");
    if (hash!=="#featured") {
       $('header nav').addClass('inbody');
@@ -51,7 +59,6 @@ $(function() {
         $('header nav').removeClass('inbody');
      }
   })
-
 
   //Use smooth scrolling
   $('a[href*=#]:not([href=#])').click(function() {
